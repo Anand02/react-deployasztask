2195c4861ffd24b7fb01d63b5de80d79464306da
Anand <anand.sg@fleximc.com>
2020-06-22 15:20:19 +0530

    Updates

---
+++ precache-manifest.2627a9f4db07035dc40b08ad94a161bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a12d38ad2f241c9aaba528b775e36d9",
+    "url": "/react-deployasztask/index.html"
+  },
+  {
+    "revision": "36b10cf0071a8df155a4",
+    "url": "/react-deployasztask/static/css/2.ec274cb8.chunk.css"
+  },
+  {
+    "revision": "51b16861defae203c8cd",
+    "url": "/react-deployasztask/static/css/main.846eedc1.chunk.css"
+  },
+  {
+    "revision": "36b10cf0071a8df155a4",
+    "url": "/react-deployasztask/static/js/2.5ab5b68c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-deployasztask/static/js/2.5ab5b68c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51b16861defae203c8cd",
+    "url": "/react-deployasztask/static/js/main.59cd7726.chunk.js"
+  },
+  {
+    "revision": "854331c32d407a0412b0",
+    "url": "/react-deployasztask/static/js/runtime-main.00f1711d.js"
+  }
+]);
